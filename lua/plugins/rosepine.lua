279c273cff3b05edb79988c01487b0ce86552fd5
BeroBrine <abhishekrana8818@gmail.com>
2024-01-16 14:26:09 +0530

    fixing some things

--- lua/plugins/rosepine.lua
+++ lua/plugins/rosepine.lua
@@ -1,7 +1,7 @@
 local config = function()
   require('rose-pine').setup({
-    variant = "dawn", -- auto, main, moon, or dawn
-    dark_variant = "dawn", -- main, moon, or dawn
+    variant = "main", -- auto, main, moon, or dawn
+    dark_variant = "main", -- main, moon, or dawn
     dim_inactive_windows = false,
     extend_background_behind_borders = true,
 
@@ -61,17 +61,10 @@ local config = function()
         -- end
     end,
 })
-
-vim.cmd('colorscheme rose-pine')
--- vim.cmd('colorscheme rose-pine-main')
--- vim.cmd('colorscheme rose-pine-moon')
--- vim.cmd('colorscheme rose-pine-dawn')
-
 end
-
-
 
 return {
   'rose-pine/neovim' ,
   name = 'rose-pine' ,
+  config = config ,
 }
